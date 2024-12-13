1b55794c2c423d940ca265f9950bdca4abfa67f0
Daniel Gray <danielgrayca925@gmail.com>
2024-12-12 18:02:09 -0800

    The first of many

---
+++ main.dart
@@ -0,0 +1,13 @@
+
+//libraries that we're going to import 
+
+//These are the libraries which we are getting sqlite from
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'dart:async';
+
+
+
+void main() async {
+
+}
