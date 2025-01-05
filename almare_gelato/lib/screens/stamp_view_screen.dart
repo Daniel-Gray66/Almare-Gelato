8a334a17c65a06780655141e05b3aa9d514d437e
rfmineguy <rfmineguy@gmail.com>
2025-01-05 15:18:43 -0800

    StampView: Created custom grapics for the cones

--- almare_gelato/lib/screens/stamp_view_screen.dart
+++ almare_gelato/lib/screens/stamp_view_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'home_screen.dart';
 import 'package:almare_gelato/widgets/customer_drawer.dart';
@@ -8,13 +11,85 @@ class StampViewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Stamp View'),
-      ),
-      drawer: const CustomDrawer(),
-      body: const Center(
-        child: Text('This is the Stamp View screen.'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Stamp View'),
+        ),
+        drawer: const CustomDrawer(),
+        body: GridView.count(
+          primary: false,
+          padding: const EdgeInsets.all(50),
+          crossAxisSpacing: 0,
+          mainAxisSpacing: 20,
+          crossAxisCount: 3,
+          children: <Widget>[
+            CustomPaint(
+              painter: ConePainter(),
+            ),
+            CustomPaint(
+              painter: ConePainter(),
+            ),
+            CustomPaint(
+              painter: ConePainter(),
+            ),
+            CustomPaint(
+              painter: ConePainter(),
+            ),
+            CustomPaint(
+              painter: ConePainter(),
+            ),
+            CustomPaint(
+              painter: ConePainter(),
+            ),
+            CustomPaint(
+              painter: ConePainter(),
+            ),
+            CustomPaint(
+              painter: ConePainter(),
+            ),
+            CustomPaint(
+              painter: ConePainter(),
+            ),
+          ],
+        )
+        // body: CustomPaint(
+        //   painter: ConePainter(),
+        //   child: Container(),
+        // ),
+        // body: const Center(
+        //   child: Text('This is the Stamp View screen.'),
+        // ),
+        );
+  }
+}
+
+class ConePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.getLocalClipBounds();
+    var paint = Paint()
+      ..color = Colors.teal
+      ..strokeWidth = 2
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round;
+
+    const radius = 23.5;
+
+    var centerx = (size.width / 2) - (radius / 2);
+    var centery = (size.height / 2) - 50;
+
+    var leftx = centerx + radius * cos(pi / 6);
+    var lefty = centery + radius * sin(pi / 6);
+
+    var rightx = centerx - radius * cos(-pi / 6);
+    var righty = centery - radius * sin(-pi / 6);
+
+    canvas.drawLine(Offset(leftx, lefty), Offset(centerx, radius * 2), paint);
+    canvas.drawLine(Offset(rightx, righty), Offset(centerx, radius * 2), paint);
+    canvas.drawCircle(Offset(centerx, centery), radius, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
   }
 }
